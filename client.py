cf08bc6122d5628d10c91a34f86beaf2668c84fb
Amikuto <56434766+Amikuto@users.noreply.github.com>
2021-03-09 12:32:32 +0300

    2: выполнены задания до пункта 7.

--- client.py
+++ client.py
@@ -2,17 +2,25 @@ import socket
 from time import sleep
 
 sock = socket.socket()
-# sock.setblocking(1)
+print("Начало соединения с сервером!")
+address = input("Введите адрес для подключения: ")
+if not address or address == '':
+    address = "localhost"
+
+port = input("Введите номер порта: ")
+if not port or port == '':
+    port = 9090
 sock.connect(('localhost', 9090))
 
-#msg = input()
-# msg = input("Введите текст эха: ")
+connection = True
 
 while True:
     send = input()
     msg = send
+    print("Началась отправка данных серверу!")
     sock.send(msg.encode())
 
+    print("Прием данных от сервера!")
     data = sock.recv(1024)
 
     # sock.close()
--- server.py
+++ server.py
@@ -1,20 +1,51 @@
+import errno
 import socket
+import logging
+
+
+logging.basicConfig(format=u'%(levelname)-8s [%(asctime)s] %(message)s', level=logging.DEBUG, filename='logs.log')
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+port = 9090
+while True:
+	try:
+		sock.bind(('', port))
+		print("Сервер запущен на порту -", port)
+		logging.info(f"Сервер запущен на порту - {port}")
+		break
+	except socket.error as e:
+		if e.errno == errno.EADDRINUSE:
+			print("Порт уже занят!")
+			port += 1
+			logging.warning(f"Порт уже занят, изменение на порт номер: {port}")
+		else:
+			print("Ошибка в подключении! ", e)
+			logging.error(f"Ошибка в подключении! {e}")
+
+sock.listen(0)
+print("Начало прослушивания порта!")
+logging.info("Начало прослушивания порта!")
 
 while True:
-	data = conn.recv(1024)
-	if data.decode() == "exit":
-		conn.close()
-		break
-	msg += data.decode()
-	conn.send(data)
+	conn, addr = sock.accept()
+	print("Пользователь подключен!")
+	print(addr)
+	logging.info(f"Пользователь подключен! {addr}")
 
-	print(msg)
+	msg = ''
 
+	while True:
+		print("Начался прием данных от клиента!")
+		logging.info("Начался прием данных от клиента!")
+		data = conn.recv(1024)
+		if data.decode() == "exit":
+			print("Завершение работы сервера!")
+			logging.info("Завершение работы сервера!")
+			conn.close()
+			break
+		msg += data.decode()
+
+		print("Началась отправка данных клиенту!")
+		logging.info(f"Началась отправка данных клиенту! {data.decode()}")
+		conn.send(data)
